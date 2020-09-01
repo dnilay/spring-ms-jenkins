4a1cec94754a18ee5853b7e14c2ba87be33314de
nilay <mailnilaydas@gmail.com>
2020-09-01 23:36:12 +0530

    commit

---
+++ src/main/java/com/example/springbootapiexample/repository/ThemeParkRideRepository.java
@@ -0,0 +1,11 @@
+package com.example.springbootapiexample.repository;
+import com.example.springbootapiexample.model.ThemeParkRide;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
