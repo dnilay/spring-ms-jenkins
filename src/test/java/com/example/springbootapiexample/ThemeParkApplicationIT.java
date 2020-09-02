d7d7f6e46c6a440531df5edb8f1e6e90985755db
nilay <mailnilaydas@gmail.com>
2020-09-02 12:15:40 +0530

    commit

--- src/test/java/com/example/springbootapiexample/ThemeParkApplicationIT.java
+++ src/test/java/com/example/springbootapiexample/ThemeParkApplicationIT.java
@@ -8,9 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
